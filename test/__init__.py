7e0ace10d3bd1aafb80b2f01a61dc15993cc53b7
Lokesh20419 <131893924+Lokesh20419@users.noreply.github.com>
2023-06-11 16:16:44 -0400

    Update __init__.py

---
+++ test/__init__.py
@@ -0,0 +1,106 @@
+from datetime import date
+
+
+class Serviceable:
+    def needs_service(self) -> bool:
+        pass
+
+
+class Car(Serviceable):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self) -> bool:
+        return self.engine.needs_service() or self.battery.needs_service()
+
+
+class Engine(Serviceable):
+    def needs_service(self) -> bool:
+        pass
+
+
+class CapuletEngine(Engine):
+    def __init__(self, last_service_mileage, current_mileage):
+        self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
+
+    def needs_service(self) -> bool:
+        # Your logic to determine if the CapuletEngine needs service
+        pass
+
+
+class SternmanEngine(Engine):
+    def __init__(self, warning_light_on):
+        self.warning_light_on = warning_light_on
+
+    def needs_service(self) -> bool:
+        # Your logic to determine if the SternmanEngine needs service
+        pass
+
+
+class WilloughbyEngine(Engine):
+    def __init__(self, last_service_mileage, current_mileage):
+        self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
+
+    def needs_service(self) -> bool:
+        # Your logic to determine if the WilloughbyEngine needs service
+        pass
+
+
+class CarFactory:
+    @staticmethod
+    def create_calliope(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+        return Car(engine, battery)
+
+    @staticmethod
+    def create_glissade(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        return Car(engine, battery)
+
+    @staticmethod
+    def create_palindrome(current_date: date, last_service_date: date, warning_light_on: bool) -> Car:
+        engine = SternmanEngine(warning_light_on)
+        battery = NubbinBattery(last_service_date, current_date)
+        return Car(engine, battery)
+
+    @staticmethod
+    def create_rorschach(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        return Car(engine, battery)
+
+    @staticmethod
+    def create_thovex(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        return Car(engine, battery)
+
+
+class Battery(Serviceable):
+    def needs_service(self) -> bool:
+        pass
+
+
+class SpindlerBattery(Battery):
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self) -> bool:
+        # Your logic to determine if the SpindlerBattery needs service
+        pass
+
+
+class NubbinBattery(Battery):
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self) -> bool:
+        # Your logic to determine if the NubbinBattery needs service
+        pass
